1cd843ba2a8292f9024ba7cb78026c21f3a1660e
matiasmelendi <mmelendi@10pines.com>
2017-09-02 13:45:25 -0300

    agregado el mailer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,10 +4,10 @@ class ApiController < ActionController::API
   }
 
   def report_bullying
-    # ApplicationMailer.send_report_bullying_email(
-    #     EMAILS_DE_ESCUELAS[1],
-    #     params[:porcentaje_de_sentimiento],
-    # ).deliver_now
+    ApplicationMailer.send_report_bullying_email(
+        EMAILS_DE_ESCUELAS[1],
+        params[:porcentaje_de_sentimiento],
+    ).deliver_now
 
     render status: :ok
   end
