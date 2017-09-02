3e97fbbded7f7ae8d2238c4922e89bbd82630123
matiasmelendi <mmelendi@10pines.com>
2017-09-02 16:41:55 -0300

    cors activado

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,6 @@
 class ApiController < ActionController::API
+  before_filter :set_cors_headers
+
   def report_bullying
     ApplicationMailer.send_report_bullying_email(receivers, report_content).deliver_now
 
@@ -13,5 +15,12 @@ class ApiController < ActionController::API
 
   def report_content
     params.permit(:curso, :quien, :sentimiento, :tipo_bullying, :descripcion)
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
