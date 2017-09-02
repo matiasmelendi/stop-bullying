1ba2eb4b01a18091f3203e39e0324da23f8222b6
matiasmelendi <mmelendi@10pines.com>
2017-09-02 15:50:31 -0300

    Armamos el template del mail, con los nuevos parametros filtrados. El mail ya tiene el header incluido.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,17 @@
 class ApiController < ActionController::API
   def report_bullying
-    ApplicationMailer.send_report_bullying_email(ENV["EMAIL_ESCUELA"].split(','), params).deliver_now
+    ApplicationMailer.send_report_bullying_email(receivers, report_content).deliver_now
 
     render status: :ok
+  end
+
+  private
+
+  def receivers
+    ENV["EMAIL_ESCUELA"].split(',')
+  end
+
+  def report_content
+    params.permit(:curso, :quien, :sentimiento, :tipo_bullying, :descripcion)
   end
 end
